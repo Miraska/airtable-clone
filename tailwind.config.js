d7169eeb04c58dbe45f955dbe4e58dcc643f0d06
joinDARK <134739429+joinDARK@users.noreply.github.com>
2024-11-24 18:30:28 +0300

    init empty project
    Просто инициализирует пустой проект, но с минимально настроенными зависимостями

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+import withMT from "@material-tailwind/react/utils/withMT";
+ 
+export default withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        dark: {
-          50: '#f7f7f8',
-          100: '#eeeef1',
-          200: '#d9d9e0',
-          300: '#b7b7c5',
-          400: '#8e8ea3',
-          500: '#6c6c85',
-          600: '#55556d',
-          700: '#454559',
-          800: '#3b3b4b',
-          900: '#27272f',
-          950: '#1a1a20',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+});
