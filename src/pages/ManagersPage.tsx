d1ac851d1eb1b4057899b578becacac1011f9ef1
joinDARK <134739429+joinDARK@users.noreply.github.com>
2024-12-01 00:24:00 +0300

    Update ManagersPage.tsx

--- src/pages/ManagersPage.tsx
+++ src/pages/ManagersPage.tsx
@@ -106,7 +106,7 @@ export const ManagersPage = () => {
               className="px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700"
               disabled={createMutation.isLoading}
             >
-              {createMutation.isLoading ? 'Сохранени...' : 'Сохранить'}
+              {createMutation.isLoading ? 'Сохранение...' : 'Сохранить'}
             </button>
           </div>
         </form>
