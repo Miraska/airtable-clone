0f2a8ab447660a83d504751e4f5bb85c0f1edbc9
Miraska <miraska.007@gmaile.com>
2024-12-08 23:29:52 +0300

    V0.0.4

--- client/src/components/CellModal.tsx
+++ client/src/components/CellModal.tsx
@@ -282,21 +282,9 @@ export const CellModal: React.FC<CellModalProps> = ({
                     console.log();
                   }}>Сохранить</Button>
                 </div>
-              )}
-              <div className="flex justify-end gap-2">
-                <Button
-                  variant="primary"
-                  className="px-4 py-2 text-sm font-medium border border-transparent rounded-md bg-red-600 hover:bg-red-700 transition-all duration-300 text-white"
-                  onClick={() => {
-                    setIsEditing(false);
-                  }}
-                >
-                  Закрыть
-                </Button>
-                <Button type="submit">Сохранить</Button>
               </div>
-            </div>
-          </form>
+            </form>
+          )
         ) : (
           <div className="space-y-4">
             <div className="bg-gray-100 dark:bg-gray-700 p-4 rounded-lg">
