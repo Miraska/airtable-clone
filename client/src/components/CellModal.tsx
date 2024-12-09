e8d528033855940b4f39d21f9dcc15e84f97d650
Miraska <miraska.007@gmaile.com>
2024-12-09 20:36:34 +0300

    V0.0.16

--- client/src/components/CellModal.tsx
+++ client/src/components/CellModal.tsx
@@ -1,4 +1,4 @@
-ximport React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Modal } from "./Modal";
 import { RelatedDataModal } from "./RelatedData";
 import {
